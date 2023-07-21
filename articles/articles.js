6d540ac3713c4d1621f4f384fffd9ac1e237b657
jhonatan <jhonatan.s.costa97@gmail.com>
2023-07-21 15:36:26 -0300

    feat(app): create articles requests and change database timezone

--- articles/articles.js
+++ articles/articles.js
@@ -1,13 +1,67 @@
 const express = require('express');
+const articlesModel = require('../articles/article-model');
+const crypto = require("crypto");
+const slugify = require("slugify");
 const router = express.Router();
 
 
-router.get('/articles', (rea, res) => {
-
+router.post('/articles/new', (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const categoryId = req.body.categoryId
+    if(title && body && categoryId){
+        articlesModel.create({
+            id: crypto.randomUUID(),
+            title: title,
+            body: body,
+            slug: slugify(title),
+            categoryId: categoryId
+        }).then(response => {
+            res.json(response);
+        }).catch(err => console.error('Error in create article', err));
+    }
 });
 
-router.get('/admin/articles/new', (rea, res) => {
+router.delete('/articles/delete/:id', (req, res) => {
+    const id = req.params.id;
+    if(id) {
+        articlesModel.destroy({
+            where: {id: id}
+        }).then((response) => {
+            res.json(response);
+        }).catch(err => console.error('Error in delete article', err));
+    }
+});
 
+router.post('/articles/edit/:id', (req, res) => {
+    const id = req.params.id;
+    const title = req.body.title;
+    const body = req.body.body;
+    const categoryId = requ.body.categoryId
+    if(id && title && body && categoryId) {
+        articlesModel.update(
+            {title: title, slug: slugify(title), body: body, categoryId: categoryId}, {where: {id: id}}
+        ).then(response => {
+            res.json(response);
+        }).catch(err => console.error('Error in update article', err));
+    }
+});
+
+router.get('/articles/:id', (req, res) => {
+    const id = req.params.id;
+    if(id) {
+        articlesModel.findByPk(id).then(article => {
+            if(article) {
+                res.json(article);
+            }
+        }).catch(err => console.error('Error in find article', err));
+    }
+});
+
+router.get('/articles', (req, res) => {
+    articlesModel.findAll().then(articles => {
+        res.json(articles);
+    }).catch(err => console.error('Error in get all article', err));
 });
 
 
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const connection = new Sequelize('blogApp', 'root', '5897', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
